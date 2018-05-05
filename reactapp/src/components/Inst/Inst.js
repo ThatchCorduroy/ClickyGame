f79771e0d89e8d2c29bfd5677da4961c058e9e24
pawhalen <pgwhal@gmail.com>
2018-05-05 16:54:23 -0400

    finally

--- reactapp/src/components/Inst/Inst.js
+++ reactapp/src/components/Inst/Inst.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Inst.css";
 
-const Inst = () => <h2 className="inst">Click and Image to Begin</h2>;
+const Inst = () => <h2 className="inst">Click any Image to Begin</h2>;
 
 export default Inst;
